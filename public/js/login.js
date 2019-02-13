66f6050871de0f38910e2351132e74db71b67b59
PrashantaReddy <prashant.puh@gmail.com>
2019-02-13 15:10:19 +0530

    fb

--- public/js/login.js
+++ public/js/login.js
@@ -1,61 +1,59 @@
 
+//   $("#fblogin").click(function () {
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: FBID,
+        cookie: true,
+        xfbml: true,
+        version: 'v3.2'
+    });
 
-$(document).ready(function () {
-    //  console.log('hhhhhhhhhh', $(".fab fa-facebook-square"))
-    console.log('FBID', FBID);
-    //   $("#fblogin").click(function () {
-    window.fbAsyncInit = function () {
-        FB.init({
-            appId: FBID,
-            cookie: true,
-            xfbml: true,
-            version: 'v3.2'
-        });
+    FB.AppEvents.logPageView();
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
 
-        FB.AppEvents.logPageView();
-        FB.getLoginStatus(function (response) {
-            statusChangeCallback(response);
-        });
+};
 
-    };
-    function statusChangeCallback(response) {
-        if (response.status === 'connected') {
-            console.log('already have an account');
-            fblogin()
-        } else {
-            FB.login(
-                function (response) {
-                    // handle the response
-                    if (response.status === "connected") {
-                        console.log("yes");
-                    } else {
-                        console.log("no");
-                    }
-                },
-                { scope: "name,email" }
-            );
-        }
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 
-    }
-    function fblogin() {
-        FB.api("/me?fields=name,email", function (response) {
-            console.log("response after login", response);
-        });
-
-    }
-    function checkLoginState() {
-        FB.getLoginStatus(function (response) {
-            statusChangeCallback(response);
-        });
+// })
+function statusChangeCallback(response) {
+    if (response.status === 'connected') {
+        console.log('already have an account');
+        fblogin()
+    } else {
+        FB.login(
+            function (response) {
+                // handle the response
+                if (response.status === "connected") {
+                    console.log("yes");
+                } else {
+                    console.log("no");
+                }
+            },
+            { scope: "name,email" }
+        );
     }
 
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    // })
-})
+
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+}
+function fblogin() {
+    FB.api("/me?fields=name,email", function (response) {
+        console.log("response after login", response);
+    });
+
+}
