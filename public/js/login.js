5a79d4457a0eceb35f9a0c04e2285c8919d65898
PrashantaReddy <prashant.puh@gmail.com>
2019-02-13 15:02:34 +0530

    fb

--- public/js/login.js
+++ public/js/login.js
@@ -3,58 +3,59 @@
 $(document).ready(function () {
     //  console.log('hhhhhhhhhh', $(".fab fa-facebook-square"))
     console.log('FBID', FBID);
-    $("#fblogin").click(function () {
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: FBID,
-                cookie: true,
-                xfbml: true,
-                version: 'v3.2'
-            });
+    //   $("#fblogin").click(function () {
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: FBID,
+            cookie: true,
+            xfbml: true,
+            version: 'v3.2'
+        });
 
-            FB.AppEvents.logPageView();
-            FB.getLoginStatus(function (response) {
-                statusChangeCallback(response);
-            });
+        FB.AppEvents.logPageView();
+        FB.getLoginStatus(function (response) {
+            statusChangeCallback(response);
+        });
 
-        };
-        function statusChangeCallback(response) {
-            if (response.status === 'connected') {
-                console.log('already have an account');
-                fblogin()
-            } else {
-                FB.login(
-                    function (response) {
-                        // handle the response
-                        if (response.status === "connected") {
-                            console.log("yes");
-                        } else {
-                            console.log("no");
-                        }
-                    },
-                    { scope: "name,email" }
-                );
-            }
-
-
+    };
+    function statusChangeCallback(response) {
+        if (response.status === 'connected') {
+            console.log('already have an account');
+            fblogin()
+        } else {
+            FB.login(
+                function (response) {
+                    // handle the response
+                    if (response.status === "connected") {
+                        console.log("yes");
+                    } else {
+                        console.log("no");
+                    }
+                },
+                { scope: "name,email" }
+            );
         }
-        function fblogin() {
-            FB.api("/me?fields=name,email", function (response) {
-                console.log("response after login", response);
-            });
 
-        }
-        function checkLoginState() {
-            FB.getLoginStatus(function (response) {
-                statusChangeCallback(response);
-            });
-        }
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) { return; }
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    })
+
+    }
+    function fblogin() {
+        FB.api("/me?fields=name,email", function (response) {
+            console.log("response after login", response);
+        });
+
+    }
+    function checkLoginState() {
+        FB.getLoginStatus(function (response) {
+            statusChangeCallback(response);
+        });
+    }
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    // })
 })
--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(function (req, res, next) {
     res.locals.error_msg = req.flash('error_msg');
     next();
 })
-
+console.log('__dirname', __dirname);
 
 module.exports = app;
 require('./init_functions');
